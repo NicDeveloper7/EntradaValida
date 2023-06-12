cf60a3c3dcacb25f7a9d82e52e94e465d6da79a4
Nicholas <nicholasdev@zohomail.com>
2023-06-11 23:21:53 -0300

    Exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿    //By NicholasDev
+    int number;
+
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine("--- Entrada Válida ---");
+    Console.ResetColor();
+
+    //Solicitação do número ao usuário
+    Console.WriteLine("Digite um número entre 1 e 9 (ou 0 para cancelar)...: ");
+    number = int.Parse(Console.ReadLine()!);
+
+    //Loop para continuar solicitando números até que seja digitado o valor 0
+    while (number != 0)
+    {
+        //Solicitação do número ao usuário
+        Console.WriteLine($"Digite um número entre 1 e 9 (ou 0 para cancelar)...:{number}");
+        number = int.Parse(Console.ReadLine()!);
+    }
+
+    //Verifica se o número digitado é igual a 0
+    if (number == 0)
+    {
+     
+        Console.WriteLine("Operação cancelada");
+        Console.WriteLine();
+
+    
+        Console.WriteLine("Volte sempre!");
+    }
